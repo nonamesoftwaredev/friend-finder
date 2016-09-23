90d76b72a3654998a86d9c2cb05de00e2d2d9f4e
Aaron Hinton <aaron@aaronhinton.net>
2016-09-22 22:42:53 -0700

    Update .yml file

---
+++ Friend Finder/Friend-FinderTests/Friend_FinderTests.swift
@@ -0,0 +1,24 @@
+//
+//  Friend_FinderTests.swift
+//  Friend FinderTests
+//
+//  Created by Aaron Hinton on 9/18/16.
+//  Copyright © 2016 No Name Software. All rights reserved.
+//
+
+import XCTest
+
+class Friend_FinderTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+    }
+    
+    override func tearDown() {
+        super.tearDown()
+    }
+    
+    func testExample() {
+        XCTAssert(true)
+    }
+}
